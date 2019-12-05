b006a59dd82f983e4c24e4d20ac8de5e8d53d18d
KYLE X NAYLOR <naylorkx@gmail.com>
2019-12-04 20:26:45 -0500

    swag

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,13 @@
-﻿using ExampleGrid.Models;
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using ExampleGrid.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Data.SQLite;
 using System.Diagnostics;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ExampleGrid.Controllers
 {
@@ -17,6 +23,158 @@ namespace ExampleGrid.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Excel123()
+        {
+            //CreateSpreadsheetWorkbook("C:\\Users\\naylo\\Downloads\\New folder\\new.xlsx");
+            Excel1();
+            return View();
+        }
+
+        public static void CreateSpreadsheetWorkbook(string filepath)
+        {
+            // Create a spreadsheet document by supplying the filepath.
+            // By default, AutoSave = true, Editable = true, and Type = xlsx.
+            SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.
+                Create(filepath, SpreadsheetDocumentType.Workbook);
+
+            // Add a WorkbookPart to the document.
+            WorkbookPart workbookpart = spreadsheetDocument.AddWorkbookPart();
+            workbookpart.Workbook = new Workbook();
+
+            // Add a WorksheetPart to the WorkbookPart.
+            WorksheetPart worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+            var sheetData = new SheetData();
+            // worksheetPart.Worksheet = new Worksheet(sheetData);
+            worksheetPart.Worksheet = new Worksheet(new SheetData());
+
+            // Add Sheets to the Workbook.
+            Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook.
+                AppendChild<Sheets>(new Sheets());
+
+            // Append a new worksheet and associate it with the workbook.
+            Sheet sheet = new Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart.
+                GetIdOfPart(worksheetPart),
+                SheetId = 1,
+                Name = "mySheet"
+            };
+
+            // Open the document for editing.
+            //using (SpreadsheetDocument spreadSheet = SpreadsheetDocument.Open(docName, true))
+            //{
+            //    // Insert other code here.
+            //}
+
+            sheets.Append(sheet);
+
+            string cs = "URI=file:CustomerDB.db";
+            string data = String.Empty;
+
+            int i = 0;
+            int j = 0;
+
+            using (SQLiteConnection con = new SQLiteConnection(cs))
+            {
+                con.Open();
+
+                string stm = "SELECT * FROM CustomerTB";
+
+                using (SQLiteCommand cmd = new SQLiteCommand(stm, con))
+                {
+                    using (SQLiteDataReader rdr = cmd.ExecuteReader())
+                    {
+                        while (rdr.Read()) // Reading Rows
+                        {
+                            Row newRow = new Row();
+                            for (j = 0; j <= rdr.FieldCount - 1; j++) // Looping throw colums
+                            {
+                                data = rdr.GetValue(j).ToString();
+                                //xlWorkSheet.Cells[i + 1, j + 1] = data;
+
+                                Cell cell = new Cell();
+                                cell.DataType = CellValues.String;
+                                cell.CellValue = new CellValue(data);
+                                newRow.AppendChild(cell);
+                            }
+                            sheetData.AppendChild(newRow);
+                            i++;
+                        }
+                    }
+                }
+                con.Close();
+            }
+
+            workbookpart.Workbook.Save();
+
+            // Close the document.
+            spreadsheetDocument.Close();
+        }
+
+        public static void Excel1()
+        {
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Create("C:\\Users\\naylo\\Downloads\\New folder\\new.xlsx", SpreadsheetDocumentType.Workbook))
+            {
+                // Add a WorkbookPart to the document.
+                WorkbookPart workbookpart = spreadsheetDocument.AddWorkbookPart();
+                workbookpart.Workbook = new Workbook();
+
+                // Add a WorksheetPart to the WorkbookPart.
+                WorksheetPart worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+                SheetData sheetData = new SheetData();
+                worksheetPart.Worksheet = new Worksheet(sheetData);
+
+                // Add Sheets to the Workbook.
+                Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook.
+                    AppendChild<Sheets>(new Sheets());
+
+                // Append a new worksheet and associate it with the workbook.
+                Sheet sheet = new Sheet()
+                {
+                    Id = spreadsheetDocument.WorkbookPart.
+                    GetIdOfPart(worksheetPart),
+                    SheetId = 1,
+                    Name = "Swag"
+                };
+
+                Row row = new Row() { RowIndex = 1 };
+                Cell header1 = new Cell() { CellReference = "A1", CellValue = new CellValue("Interval Period Timestamp"), DataType = CellValues.String };
+                row.Append(header1);
+                Cell header2 = new Cell() { CellReference = "B1", CellValue = new CellValue("Settlement Interval"), DataType = CellValues.String };
+                row.Append(header2);
+                Cell header3 = new Cell() { CellReference = "C1", CellValue = new CellValue("Aggregated Consumption Factor"), DataType = CellValues.String };
+                row.Append(header3);
+                Cell header4 = new Cell() { CellReference = "D1", CellValue = new CellValue("Loss Adjusted Aggregated Consumption"), DataType = CellValues.String };
+                row.Append(header4);
+
+                sheetData.Append(row);
+
+                sheets.Append(sheet);
+
+                workbookpart.Workbook.Save();
+
+                // Close the document.
+                spreadsheetDocument.Close();
+            }
+        }
+
+        private static void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+            }
+            finally
+            {
+                GC.Collect();
+            }
         }
 
         public IActionResult Privacy()
